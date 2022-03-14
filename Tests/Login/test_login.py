acabe1a5e61949862e7f63138b8c4a33cee52d16
adityaTask <Killedarrox@1>
2022-03-14 22:19:55 +0530

    modified test_login order

--- Tests/Login/test_login.py
+++ Tests/Login/test_login.py
@@ -16,12 +16,14 @@ class LoginTest(unittest.TestCase):
 
     @data(*get_csv_data(get_proj_dir() + '\\HRM\\Tests\\Login\\test_valid_user.csv'))
     @unpack
+    @pytest.mark.run(order=2)
     def test_valid_login(self, username, password):
         self.lp.enter_user_name(username)
         self.lp.enter_password(password)
         self.lp.click_login_button()
         assert self.lp.check_valid_login()
 
+    @pytest.mark.run(order=1)
     def test_invalid_login(self, username="Admin1", password='admin123'):
         self.lp.logout()
         self.lp.enter_user_name(username)
