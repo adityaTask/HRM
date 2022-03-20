e767bf45c9cb63bd5aa0129203d052ed3159cfd0
adityaTask <Killedarrox@1>
2022-03-20 17:08:16 +0530

    modified find and check row method , so that it doesn't run without stopping

--- Tests/Admin/test_user_management.py
+++ Tests/Admin/test_user_management.py
@@ -14,7 +14,7 @@ class UserTest(unittest.TestCase):
         self.um = UserManagement(setup)
 
     now = datetime.datetime.now()
-    @data(['ESS','Orange Test','orange'+str(now.hour)+str(now.min),'Enabled','orange123'])
+    @data(['ESS','Orange Test','zorange'+str(now.hour)+str(now.min),'Enabled','orange123'])
     @unpack
     def test_user(self,role, emp_name, username, status, password):
         self.um.navigate_to_users()
--- base/common.py
+++ base/common.py
@@ -9,6 +9,7 @@ class Common(SeleniumDriver):
         self.rows_in_table_by_xpath = '//tbody//tr'
         self.item_in_table_by_xpath = '//tbody//tr[{0}]//td[{1}]//a'
         self.check_row_by_xpath = "//tbody//tr[{0}]//td[1]//input"
+        self.page_top_by_xpath = "//ul[@class='paging top']//li[@class='desc']"
 
     def find_column_index_in_table(self,column_name):
         column_list = self.find_elements_list(self.columns_by_xpath, "xpath")
@@ -20,14 +21,23 @@ class Common(SeleniumDriver):
 
     def find_row_index_in_table(self, column_name, value):
         col_index = self.find_column_index_in_table(column_name)
-        row_index = 1
-        while True:
+        page_by_top = self.find_elements_list(self.page_top_by_xpath,"xpath")
+        if len(page_by_top) > 0:
+            text = self.get_text(self.page_top_by_xpath)
+            rows = text.split(' of ')[-1]
+            pages = (rows//50) + 1
+        else:
+            pages = 1
+        current_page = 1
+        while current_page <= pages:
+            row_index = 1
             for i in range(1, len(self.find_elements_list(self.rows_in_table_by_xpath, "xpath")) + 1):
                 current_name = self.get_text(self.item_in_table_by_xpath.format(i, col_index),"xpath")
                 if current_name == value:
                     return row_index
                 row_index += 1
             self.element_click(self.next_page_by_xpath, "xpath")
+            current_page += 1
 
     def find_and_check_row_in_table(self, column_name, value):
         row_index = self.find_row_index_in_table(column_name, value)
